f91159c55a1d98c51b4b0ac1504ec1488474f09c
Your Name <kybinz@gmail.com>
2013-03-05 10:37:24 +0900

    add maketree function : checkConflict

--- maketree.py
+++ maketree.py
@@ -4,6 +4,24 @@ import re
 import sys
 import posixpath as path
 
+def __checkConflict(treename, treedict, recursionlist=[]):
+	if treename in recursionlist:
+		recursionlist.append(treename)
+		print('error : recursion conflict!')
+		print(' -> '.join(recursionlist))
+		sys.exit(1)
+
+	recursionlist.append(treename)
+	branches = [i.rstrip() for i in treedict[treename].splitlines() if i.strip()]
+	for b in branches:
+		b = b.strip()
+		if b.startswith('@'):
+			treename = b.replace('@', '', 1)
+			if  treename in treedict:
+				__checkConflict(treename, treedict, recursionlist)
+	return
+
+
 def __parseTree(treestr):
 	branches = [i.rstrip() for i in treestr.splitlines() if i.strip()]
 	defaultdepth = branches[0].count('\t') # expand to spaces
@@ -11,17 +29,23 @@ def __parseTree(treestr):
 	dirs= []
 	tree = []
 	for b in branches:
-		dir = b.strip()
+		dirname = b.strip()
 		depth = b.count('\t')-defaultdepth
 
-		if depth < 0 or depth - len(dirs)  > 0: 
-			# if depth is below zero
-			# or "jumps forward" more than one step, then raise error
+		if depth < 0: # if depth is below zero, raise error
 			print('please check your tree : {0}'.format(path.join(*dirs)))
-			raise IndexError
+			sys.exit(1)
 
 		dirs = dirs[:depth]
-		dirs.append(dir)
+		dirs.append(dirname)
+
+		try:	
+			dirs[depth] 
+		except: 
+			# depth != len(dirs[:depth]) + 1
+			# means depth jump forward more than 1 step
+			print('please check your tree : {0}'.format(path.join(*dirs)))
+			raise IndexError
 
 		tree.append(path.join(*dirs))
 	return tree
@@ -46,25 +70,25 @@ def __parseFile(text):
 
 	return treedict
 
-def make(tree, dir):
+
+def make(treename, dir):
 	thisfilepath = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/')
 	treefile = path.join(thisfilepath, 'folder_tree.txt')
 	with open(treefile) as f:
 		text = f.read()
 
 	treedict = __parseFile(text)
-	treestr = treedict[tree]
+	
+	__checkConflict(treename, treedict)	
+	
+	treestr = treedict[treename]
 	if treestr:
 		branches = __parseTree(treestr)
 
-	if branches:
-		for b in branches:
+		if branches:
+			for b in branches:
 				print(path.join(dir, b))
-				os.makedirs(path.join(dir, b))
-	# elif branches is False:
-	# 	print('Check tree "{name}"'.format(name=name))
-	# elif branches is None:
-	# 	print('No branches at "{name}"'.format(name=name))
+				# os.makedirs(path.join(dir, b))
 	
 
 if __name__=="__main__":
