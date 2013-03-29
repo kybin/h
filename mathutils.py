cdca811317222b54890d3a42215ecce187b9fd25
kim yongbin <kybinz@gmail.com>
2013-03-30 01:43:13 +0900

    fix line intersection func

--- mathutils.py
+++ mathutils.py
@@ -4,7 +4,10 @@ def lineIntersectionPoint((x1, y1, x2, y2), (x3, y3, x4, y4)):
 		L2 = ((x3,y3), (x4,y4))
 		http://en.wikipedia.org/wiki/Line-line_intersection
 	'''
-	x = ((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4))
-	y = ((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4))
+	denominator = ((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4))
+	if denominator is 0:
+		return
+	x = ((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4))/denominator
+	y = ((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4))/denominator
 	return x, y
 
