a8ac399754c4fadef2e86cc3673f3fc6a1b4edd5
kim yongbin <kybinz@gmail.com>
2014-01-27 08:22:10 +0900

    split softwareinfo to softwareinfo.py

--- h.py
+++ h.py
@@ -13,6 +13,7 @@ from subprocess import call
 import env
 import maketree
 import filebox
+import softwareinfo
 from OrderedDict import OrderedDict
 
 class shotdata:
@@ -22,55 +23,7 @@ class shotdata:
 		self.user = getUser()
 		self.rootpath = env.ProjectRoot
 		self.workingdir = self.rootpath
-		self.software = {
-			'houdini':
-			{'dir':'scenes', 'read':['hip'], 'write':'hip', 'batch':'hython', 'execute': 'houdini',
-			'initscript' : '''
-hou.hipFile.clear()
-
-hou.setFrame({fps})
-hou.hscript('tset {start}, {end}')
-
-ip = hou.node('out').createNode('ifd', 'ip')
-ip.parm('vm_picture').set('ip')
-
-seq = hou.node('out').createNode('ifd', '{shot}_{task}')
-seq.parm('vm_picture').set('$OUT/v01/$OS.$F4.exr')
-seq.parm('vm_image_comment').set('$HIP/$HIPNAME')
-seq.setPosition(hou.Vector2(2,0))
-
-hou.hscript('set -g SHOW = {show}')
-hou.hscript('set -g SEQ = {seq}')
-hou.hscript('set -g SCENE = {scene}')
-hou.hscript('set -g SHOT = {shot}')
-hou.hscript('set -g TASK = {task}')
-
-hou.hscript('set -g SHOWPATH = {showpath}')
-hou.hscript('set -g SEQPATH = {seq}')
-hou.hscript('set -g SCENEPATH = {scenepath}')
-hou.hscript('set -g SHOTPATH = {shotpath}')
-hou.hscript('set -g TASKPATH = {taskpath}')
-hou.hscript('set -g DATAPATH = {showpath}/assets')
-
-hou.hscript('set -g JOB = {shotpath}')
-hou.hscript('set -g OUT = {renderpath}')
-
-hou.hipFile.save('{filepath}')'''
-			},
-
-			'maya':
-			{'dir':'scenes', 'read':['ma', 'mb'], 'write':'mb', 'batch':'mayabatch.exe -script', 'execute': 'maya.exe',
-			'initscript':'''
-file -rn "{file}";
-file -s;'''
-			},
-
-			'max':
-			{'dir':'scenes', 'read':['max'], 'write':'max', 'batch':'3dsmax.exe -mxs', 'execute': '3dsmax',
-			'initscript':'''
-'''			}
-		}
-
+		self.software = softwareinfo.softwareinfo
 		self.use = 'houdini'
 		self.renderdir = 'render'
 		self.structfile = '.showinfo'
